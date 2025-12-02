43b01f7d21917b31081ef836dad4936ce075c7af
Paulina <paulinainf2002@gmail.com>
2025-12-02 17:36:53 +0100

    Fix cart image

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -52,7 +52,7 @@
             <div class="cart-item">
 
                 <div class="cart-thumb">
-                    <img src="{{ Storage::url($product->image) }}" alt="">
+                    <img src="{{ Storage::url($item['image']) }}" alt="">
                 </div>
 
                 <div class="cart-info">
