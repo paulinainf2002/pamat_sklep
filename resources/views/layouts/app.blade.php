6388b758ac9219abc493101b1d2e95dba7f33b41
Paulina <paulinainf2002@gmail.com>
2026-01-03 16:00:53 +0100

    dodano znacznik meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta
+         name="description"
+         content="Sklep PaMat - Wybierz swoją ulubioną mieszankę, poznaj ciekawe smaki i zamów online z szybką dostawą.">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
