7ebd53c5654dfd1389e0566a1d7015e77cf23ac6
Paulina <paulinainf2002@gmail.com>
2026-01-06 14:03:55 +0100

    P24 fix4

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'p24' => [
     'merchant_id' => env('P24_MERCHANT_ID'),
     'pos_id'      => env('P24_POS_ID'),
-    'api_key'     => env('P24_API_KEY'),
+    'api_key'     => env('P24_REPORT_KEY'),
     'crc'         => env('P24_CRC'),
     ],
 
